89e581bcec31b5803f43c0d1b28cb13b88673a15
ElCodMaler <gilbertojeda30@gmail.com>
2024-10-08 21:32:43 -0400

    limpiando codigo 1

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { Menu } from './components/layout'
 import { Inicio } from './pages/Inicio';
+import './styles/output.css'
 
 export default function App() {
   return (
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import tailwindcss from 'tailwindcss';
+import autoprefixer from 'autoprefixer';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: "https://ElCodMaler.github.io/style-up-test",
+  css: {
+    postcss: {
+      plugins: [
+        tailwindcss,
+        autoprefixer,
+      ],
+    },
+  }
 })
